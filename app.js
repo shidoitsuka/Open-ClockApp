ad19cf02c5b53c8286786c5ce2ab14d07c7c60a0
Brian Harianja <hi@shdev.tech>
2021-10-11 03:40:32 +0700

    showing day of the week

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const clock = document.querySelector('.clock');
+const dayEl = document.querySelector('.day');
 
 const currentTime = () => {
     const now = new Date();
 
-    const hour = now.getHours();
-    const min = now.getMinutes();
-    const sec = now.getSeconds();
+    var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+    const day = days[now.getDay()];
+    const hour = `${now.getHours() < 10 ? "0" + now.getHours() : now.getHours()}`;
+    const min = `${now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes()}`;
+    const sec = `${now.getSeconds() < 10 ? "0" + now.getSeconds() : now.getSeconds()}`;
 
     console.log(hour, min, sec);
 
@@ -13,7 +17,9 @@ const currentTime = () => {
     <span>${hour}</span>:
     <span>${min}</span>:
     <span>${sec}</span>
-    `
+    `;
+
+    dayEl.innerHTML = day;
     clock.innerHTML = show;
 };
 
